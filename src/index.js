ea2cca3ee3ecc05c491a11fa489ab6c6e48ec78b
ivan-pogorelov <ivanpogorelov97a@gmail.com>
2021-08-19 21:31:14 +0300

    made the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    
+    let numberString = n.toString();
+	let numberStringToArr = numberString.split('');
+
+	if (numberStringToArr[0] === '-') {
+		return numberStringToArr.reverse().join('').substring(0, numberString.length - 1);
+	}
+
+	return numberStringToArr.reverse().join('');
 }
